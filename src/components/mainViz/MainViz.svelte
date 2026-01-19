69a6317b117475bfbeaf18c3d957deafcfcc23b1
Anne-Marie Dufour <annema.dufour@gmail.com>
2026-01-19 17:43:15 +0100

    Update packages, install tailwind

---
+++ src/components/mainViz/MainViz.svelte
@@ -0,0 +1,3 @@
+<svg viewBox="0 0 1600 1000" style="border: 1px solid cyan;">
+  <circle cx="50" cy="50" r="40" fill="#1a73e8" />
+</svg>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import MainViz from '../components/mainViz/MainViz.svelte';
+</script>
+
+<main>
+  <div class="container">
+    <MainViz />
+  </div>
+</main>
