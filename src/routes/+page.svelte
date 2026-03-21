f2e3b04c807773d09265825926a39fc873a01ac3
Anne-Marie Dufour <annema.dufour@gmail.com>
2026-03-21 11:35:21 +0100

    Add footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import Navbar from '../components/Navbar.svelte';
   import Intro from '../components/Intro.svelte';
   import Context from '../components/context/Context.svelte';
+  import Footer from './Footer.svelte';
 </script>
 
 <header class="sticky top-0 z-50 bg-white/60 backdrop-blur-sm border-b border-white/20">
@@ -10,9 +11,8 @@
 <main>
   <div class="container">
     <Intro />
-    <section id="exploratory-journey"></section>
-    <section id="context">
-      <Context />
-    </section>
+
+    <Context />
   </div>
 </main>
+<Footer />
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,13 @@
+<script>
+  import LogoFooter from '$lib/assets/logo_footer.png';
+</script>
+
+<footer class="container mt-15 mb-4">
+  <div class="inner-container">
+    <div class="flex flex-col items-center text-center gap-4 md:flex-row md:justify-between">
+      <img src={LogoFooter} alt="Shomrim's logo" class="w-30 h-auto" />
+      <div>© Copyright The Center for Media and Democracy Inc, non for profit</div>
+      <a href="mailto:info@hashomrim.org?subject=contact%20us">Contact Us</a>
+    </div>
+  </div>
+</footer>
