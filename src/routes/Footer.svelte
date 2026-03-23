eced5e530069624880e37e3caad4e69a67ccc092
Anne-Marie Dufour <annema.dufour@gmail.com>
2026-03-23 13:54:23 +0100

    Reduce content width

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -2,10 +2,14 @@
   import LogoFooter from '$lib/assets/logo_footer.png';
 </script>
 
-<footer class="container mt-15 mb-4">
+<footer class="container mt-15 mb-1">
   <div class="inner-container">
-    <div class="flex flex-col items-center text-center gap-4 md:flex-row md:justify-between">
-      <img src={LogoFooter} alt="Shomrim's logo" class="w-30 h-auto" />
+    <div
+      class="flex flex-col items-center text-center gap-4 md:flex-row md:justify-between text-sm"
+    >
+      <a href="https://www.shomrim.news/english" target="_blank">
+        <img src={LogoFooter} alt="Shomrim's logo" class="w-24 h-auto" />
+      </a>
       <div>© Copyright The Center for Media and Democracy Inc, non for profit</div>
       <a href="mailto:info@hashomrim.org?subject=contact%20us">Contact Us</a>
     </div>
